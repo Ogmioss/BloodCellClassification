6079cdd2679563e34e912293cfb3a6d1a01df961
YasBM <yasminebenmiled99@yahoo.fr>
2025-10-21 11:07:24 +0200

    yas : ajout streamlit

---
+++ Streamlit/pages/5_Ameliorations_potentielles.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.title("🚀 Pistes d'amélioration du modèle")
+
+
+st.markdown("""
+### Idées d’amélioration :
+- Augmentation de données plus riche (rotation, zoom, contraste)
+- Utiliser des **techniques d’interprétabilité** (Grad-CAM, LIME)
+- Équilibrage de classes (oversampling ou focal loss)
+- Entraînement sur GPU (Google Colab, Kaggle, etc.)
+- Optimisation des hyperparamètres (Keras Tuner, Optuna)
+""")
